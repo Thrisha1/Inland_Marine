26d88b342045a8c799cfbaa35fdec7aa419ec7b0
AB7zz <abhinavcv007@gmail.com>
2023-08-30 22:49:44 +0530

    navbar image not appearing

--- src/components/app/Navbar.jsx
+++ src/components/app/Navbar.jsx
@@ -1,41 +1,86 @@
+"use client";
+
 import React from 'react'
 import Image from 'next/image'
 
 const Navbar = () => {
-  return (
-    
-<nav className="bg-[#F9F9F9] border-[#F9F9F9]">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" className="flex items-center">
-        <Image src="/logo.png" className="h-8 mr-3" alt="Inline Marine Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#1E3888]">INLINE MARINE</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-[#F9F9F9] border-[#F9F9F9]">
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-[#1E3888] md:dark:hover:bg-transparent">Home</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-[#1E3888] md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-[#1E3888] md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-[#1E3888] md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  const [navbar, setNavbar] = React.useState(false);
 
-  )
+    return (
+        <nav className="w-full bg-[#F9F9F9] shadow">
+            <div className="justify-between px-4 md:items-center md:flex md:pl-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                      <div className='flex justify-center items-center'>
+                        <Image src="/logo.png" width="150" height="150" />
+                        <a href="javascript:void(0)">
+                            <h2 className="text-2xl font-bold text-[#1E3888] poppins">INLAND <br /> MARINE</h2>
+                        </a>
+                      </div>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className={`flex-1 justify-self-center md:block pb-3 mt-8 md:pb-0 md:mt-0 ${
+                        navbar ? "block" : "hidden"
+                    }`}
+                >
+                    <ul className="items-center justify-evenly space-y-8 md:flex md:space-x-6 md:space-y-0">
+                        <li className="text-[#1E3888] cursor-pointer poppins font-bold hover:text-white hover:bg-[#1E3888] rounded-[5px] text-center px-3 py-1">
+                            <a href="javascript:void(0)">Home</a>
+                        </li>
+                        <li className="text-[#1E3888] cursor-pointer poppins font-bold hover:text-white hover:bg-[#1E3888] rounded-[5px] text-center px-3 py-1">
+                            <a href="javascript:void(0)">About</a>
+                        </li>
+                        <li className="text-[#1E3888] cursor-pointer poppins font-bold hover:text-white hover:bg-[#1E3888] rounded-[5px] text-center px-3 py-1">
+                            <a href="javascript:void(0)">Design</a>
+                        </li>
+                        <li className="text-[#1E3888] cursor-pointer poppins font-bold hover:text-white hover:bg-[#1E3888] rounded-[5px] text-center px-3 py-1">
+                            <a href="javascript:void(0)">Services</a>
+                        </li>
+                        <li className="text-[#1E3888] cursor-pointer poppins font-bold hover:text-white hover:bg-[#1E3888] rounded-[5px] text-center px-3 py-1">
+                            <a href="javascript:void(0)">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar
