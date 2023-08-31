71af9040e6c2de8dafe9534443dca19824183686
AB7zz <abhinavcv007@gmail.com>
2023-08-31 19:38:41 +0530

    about init

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/app/Navbar"
 import Carousel from "@/components/app/Carousel"
 import FeaturedBoats from "@/components/app/FeaturedBoats"
 import Interior from "../components/app/Interior"
+import Aboutus from "@/components/app/Aboutus"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Carousel />
       <FeaturedBoats />
       <Interior />
+      <Aboutus />
     </div>
   )
 }
