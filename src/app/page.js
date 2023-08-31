1a40833e28e10f968bf4ca5b7fc46dac0dc18201
AB7zz <abhinavcv007@gmail.com>
2023-08-31 19:34:25 +0530

    started interior

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/app/Navbar"
 import Carousel from "@/components/app/Carousel"
 import FeaturedBoats from "@/components/app/FeaturedBoats"
+import Interior from "../components/app/Interior"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Carousel />
       <FeaturedBoats />
+      <Interior />
     </div>
   )
 }
