d6f8e5c371c50ca180fe4cb2b49f1be2146e2a89
AB7zz <abhinavcv007@gmail.com>
2023-08-31 19:48:18 +0530

    about us 99% done

--- src/components/app/Aboutus.jsx
+++ src/components/app/Aboutus.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Aboutus = () => {
   return (
-    <div>Aboutus</div>
+    <div className='mt-20 px-16'>
+        <h1 className='text-3xl !font-[900] poppins text-center text-[#1E3888]'>ABOUT US</h1>
+        <div className='mt-20'>
+            <p className='poppins text-[#0B0B0B] px-20 text-center'>At Inland Marine, our story is etched by the waters that have shaped our journey—a tale woven through the intricate art of maritime craftsmanship. Established with a reverence for tradition and an unwavering commitment to excellence, we stand as custodians of the rich legacy that graces Kerala's waterways. Steeped in the spirit of the backwaters, our journey began as a humble tribute to the age-old techniques of boat building. With every plank, every joint, and every contour, we embarked on a mission to revive the magnificence of Kettuvallams—traditional houseboats that carry within them the soul of our culture.
+Guided by the hands of seasoned artisans, our creations echo the whispers of ancient boatyards. Our commitment goes beyond the surface; it delves deep into the heart of every vessel we craft. Each Inland Marine Kettuvallam is a symphony, an embodiment of the timeless connection between craftsmanship and the gentle rhythms of water. As we breathe life into the timber and shape dreams upon the waves, we invite you to join us on a voyage through tradition and innovation.
+Welcome to Inland Marine, where the tides of tradition and the winds of innovation converge to create a fleet of vessels that encapsulate the heart of Kerala's waterborne narrative. Together, let us navigate the currents of authenticity and embark on a journey that resonates with the past, while sailing confidently into the future."</p>
+        </div>
+        <div className='mt-16 flex justify-center'>
+            <button className='bg-[#1E3888] px-5 py-2 text-white align-center rounded-[20px] font-bold poppins'>CONTACT US</button>
+        </div>
+    </div>
   )
 }
 
