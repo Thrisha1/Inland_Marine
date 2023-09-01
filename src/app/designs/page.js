96dce67a7cb548badf77ef72335398993c1da6a8
AB7zz <abhinavcv007@gmail.com>
2023-09-01 13:21:08 +0530

    finished contactus part too, responsive

--- src/app/designs/page.js
+++ src/app/designs/page.js
@@ -1,11 +1,13 @@
 import FeaturedBoats from "@/components/designs/featuredboats";
 import InteriorDesigns from "@/components/designs/InteriorDesigns";
+import ContactUs from "@/components/designs/ContactUs";
 
 export default function Design(){
     return(
         <div>
             <FeaturedBoats />
             <InteriorDesigns />
+            <ContactUs />
         </div>
     )
 }
