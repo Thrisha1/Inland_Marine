11db0c2e7f336bd25b75475d09e8ada47abef9b1
AB7zz <abhinavcv007@gmail.com>
2023-09-06 20:51:21 +0530

    app/designs latest

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Carousel from "@/components/app/Carousel"
 import FeaturedBoats from "@/components/app/FeaturedBoats"
 import Interior from "../components/app/Interior"
+// import PropertySlider from "@/components/app/Carousel"
 
 export default function Home() {
   return (
     <div>
-      <Carousel />
+      {/* <PropertySlider /> */}
       <FeaturedBoats />
       <Interior />
     </div>
--- src/components/app/Carousel.jsx
+++ src/components/app/Carousel.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+// "use client";
+// import React, { useEffect } from "react"
+// import Glide from "@glidejs/glide"
+// import Image from "next/image"
 
-const Carousel = () => {
-  return (
-    <div>Carousel</div>
-  )
-}
+// export default function PropertySlider({}) {
+//   useEffect(() => {
+//     const slider = new Glide(".glide-02", {
+//       type: "slider",
+//       focusAt: "center",
+//       perView: 1,
+//       autoplay: 3500,
+//       animationDuration: 700,
+//       gap: 0,
+//       classes: {
+//         nav: {
+//           active: "[&>*]:bg-gray-700",
+//         },
+//       },
+//     }).mount()
 
-export default Carousel
+//     return () => {
+//       slider.destroy()
+//     }
+//   }, [])
+
+//   return (
+//     <>
+//       <div className="relative w-full glide-02 h-max">
+//         <div className="overflow-hidden rounded-t-[20px]" data-glide-el="track">
+//           <ul className="whitespace-no-wrap flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] relative flex w-full overflow-hidden p-0">
+//             <li>
+//               <Image
+//                 width={200}
+//                 height={200}
+//                 src={"https://Tailwindmix.b-cdn.net/image-03.jpg"}
+//                 className="w-full max-w-full max-h-full m-auto"
+//               />
+//             </li>
+//             <li>
+//               <Image
+//                 width={200}
+//                 height={200}
+//                 src={"https://Tailwindmix.b-cdn.net/image-04.jpg"}
+//                 className="w-full max-w-full max-h-full m-auto"
+//               />
+//             </li>
+//             <li>
+//               <Image
+//                 width={200}
+//                 height={200}
+//                 src={"https://Tailwindmix.b-cdn.net/image-05.jpg"}
+//                 className="w-full max-w-full max-h-full m-auto"
+//               />
+//             </li>
+//             <li>
+//               <Image
+//                 width={200}
+//                 height={200}
+//                 src={"https://Tailwindmix.b-cdn.net/image-01.jpg"}
+//                 className="w-full max-w-full max-h-full m-auto"
+//               />
+//             </li>
+//             <li>
+//               <Image
+//                 width={200}
+//                 height={200}
+//                 src={"https://Tailwindmix.b-cdn.net/image-02.jpg"}
+//                 className="w-full max-w-full max-h-full m-auto"
+//               />
+//             </li>
+//           </ul>
+//         </div>
+//         <div
+//           className="absolute bottom-0 flex items-center justify-center w-full gap-2"
+//           data-glide-el="controls[nav]"
+//         >
+//           <button
+//             className="p-4 group"
+//             data-glide-dir="=0"
+//             aria-label="goto slide 1"
+//           >
+//             <span className="block w-2 h-2 transition-colors duration-300 rounded-full bg-white/20 ring-1 ring-slate-700 focus:outline-none"></span>
+//           </button>
+//           <button
+//             className="p-4 group"
+//             data-glide-dir="=1"
+//             aria-label="goto slide 2"
+//           >
+//             <span className="block w-2 h-2 transition-colors duration-300 rounded-full bg-white/20 ring-1 ring-slate-700 focus:outline-none"></span>
+//           </button>
+//           <button
+//             className="p-4 group"
+//             data-glide-dir="=2"
+//             aria-label="goto slide 3"
+//           >
+//             <span className="block w-2 h-2 transition-colors duration-300 rounded-full bg-white/20 ring-1 ring-slate-700 focus:outline-none"></span>
+//           </button>
+//           <button
+//             className="p-4 group"
+//             data-glide-dir="=3"
+//             aria-label="goto slide 4"
+//           >
+//             <span className="block w-2 h-2 transition-colors duration-300 rounded-full bg-white/20 ring-1 ring-slate-700 focus:outline-none"></span>
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
