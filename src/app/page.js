68e0ae3a1c450f282c32ca575cddd33607831e52
AB7zz <abhinavcv007@gmail.com>
2023-09-08 17:31:12 +0530

    first carousel completed, and responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import React from "react"
-import Carousel from "@/components/app/Carousel_slider"
 import FeaturedBoats from "@/components/app/FeaturedBoats"
 import Interior from "../components/app/Interior"
 import CarouselSlider from "../components/app/Carousel_slider"
 
 export default function Home() {
-  // const imgs = [img, img, img]
   return (
     <div>
       <CarouselSlider />
--- src/components/app/Carousel_slider.jsx
+++ src/components/app/Carousel_slider.jsx
@@ -7,13 +7,13 @@ import Image from "next/image";
 
 const PropertyImages = [
   {
-    image_url: "/images/designs/featuredboats/boat1/boat11.png",
+    image_url: "/images/app/featuredboats/boat1.png",
   },
   {
-    image_url: "/images/designs/featuredboats/boat1/boat12.png",
+    image_url: "/images/app/featuredboats/boat2.png",
   },
   {
-    image_url: "/images/designs/featuredboats/boat1/boat13.png",
+    image_url: "/images/app/featuredboats/boat3.png",
   },
 ];
 
@@ -34,8 +34,10 @@ const Carousel_slider = () => {
   }, [PropertyImages]);
 
   return (
-    <div className="">
-      <h1>Slider</h1>
+    <div className="md:px-20 px-5 py-5">
+      <div className="flex justify-center my-10">
+        <h1 className="text-[#1E3888] text-center text-3xl md:text-4xl poppins font-bold">Building Dreams on Water: Traditional Houseboats Redefined</h1>
+      </div>
       <Carousel
        selectedItem={activeIndex}
        autoPlay={false} // Disable the internal autoplay
@@ -46,6 +48,7 @@ const Carousel_slider = () => {
        showStatus={false}
        swipeable={true}
        dynamicHeight={false} // Adjust carousel height to content
+       className="rounded-[40px]"
       >
         {PropertyImages?.map((item) => (
           <div>
@@ -53,11 +56,15 @@ const Carousel_slider = () => {
               width={200}
               height={200}
               src={item.image_url}
-              className="w-full max-w-full max-h-full m-auto"
+              className="w-full max-w-full h-[350px] md:h-[500px] m-auto rounded-[40px]"
             />
           </div>
         ))}
       </Carousel>
+      <div className="flex flex-col justify-center relative bottom-[95px] md:bottom-[85px] z-10">
+        <h3 className="text-center text-white poppins font-bold mb-5 text-xl md:text-2xl">Embark on our Jounrey with Our Traditional Creations</h3>
+        <button className="bg-[#1E3888] flex m-auto text-white rounded-[30px] px-10 py-2 md:py-3 poppins border-4 border-white font-bold">EXPLORE</button>
+      </div>
     </div>
   );
 };
