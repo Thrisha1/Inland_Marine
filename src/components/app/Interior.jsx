cceb87373564451ed0d6c52cc3453011a607ff05
AB7zz <abhinavcv007@gmail.com>
2023-09-12 00:28:22 +0530

    interior sanity

--- src/components/app/Interior.jsx
+++ src/components/app/Interior.jsx
@@ -4,10 +4,26 @@ import interior1 from '../../../public/images/app/interior/interior1.png'
 import interior2 from '../../../public/images/app/interior/interior2.png'
 import interior3 from '../../../public/images/app/interior/interior3.png'
 import { Headline } from './common_components'
+import { useStateContext } from '@/context/StateContext'
+import { client } from '../../../sanity/lib/client'
 
 
 
 const Interior = () => {
+  const {interiors, setInteriors} = useStateContext()
+  React.useEffect(() => {
+    const query = `*[_type == "interior"] {
+      img
+    }`
+
+    client
+      .fetch(query)
+      .then(data => {
+          setInteriors(data)
+          console.log(data)
+      })
+      .catch(error => console.error(error))
+  }, [])
   return (
     <div className='my-20 px-16'>
         <div className='bg-white rounded-[25px] shadow-lg py-5'>
@@ -16,7 +32,6 @@ const Interior = () => {
                 <Image src={interior1} alt="Interior 1" className='mr-5' width="450" height="300" />
                 <Image src={interior2} alt="Interior 1" className='mr-5' width="450" height="300" />
                 <Image src={interior3} alt="Interior 1" className='' width="450" height="300" />
-                {/* <Image src={interior2} alt="Interior 1" className='mr-5' width="450" height="300" /> */}
             </div>
             <div className='mt-20'>
                 <p className='px-32 poppins text-[#0B0B0B] text-center '>As you step indoors, a realm unfurls where tradition converges with innovation, marrying hand-carved embellishments seamlessly with modern conveniences.</p>
