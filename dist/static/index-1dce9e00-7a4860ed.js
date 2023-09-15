b761bfc72a664976e14eadaf9e1eb3ddb0f31ae9
AB7zz <abhinavcv007@gmail.com>
2023-09-15 17:32:31 +0530

    sanity

--- dist/static/index-1dce9e00-7a4860ed.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-504fa488-95d77720.js";import{D as m}from"./sanity-c796ece9.js";import"./json-inspector-1cff88c5.js";export{m as DocumentPaneProvider,t as default};
--- sanity/schema.js
+++ sanity/schema.js
@@ -1,7 +1,12 @@
-import featuredBoats from './schemas/featuredBoats'
+// import blockContent from './defaultschemas/blockContent'
+// import category from './defaultschemas/category'
+// import post from './defaultschemas/post'
+// import author from './defaultschemas/author'
+
 import testimonials from './schemas/testimonials'
+import featuredBoats from './schemas/featuredBoats'
 import interior from './schemas/interior'
 
 export const schema = {
-  types: [featuredBoats, testimonials, interior]
+  types: [testimonials, featuredBoats, interior],
 }
