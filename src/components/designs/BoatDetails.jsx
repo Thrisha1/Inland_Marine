d088ff2d4debfbd66ec5a5efcd39762e66679ded
GalahadTheKnight <truceinablackhole@gmail.com>
2023-09-17 13:22:52 +0530

    fixed

--- src/components/designs/BoatDetails.jsx
+++ src/components/designs/BoatDetails.jsx
@@ -4,15 +4,50 @@ import { usePathname } from 'next/navigation';
 import boat1 from '../../../public/images/app/featuredboats/boat1.png'
 import Image from 'next/image'
 import { useStateContext } from '@/context/StateContext';
+import { client } from '../../../sanity/lib/client';
+import { UisStar } from '@iconscout/react-unicons-solid'
+
+
 
 const BoatDetails = () => {
     const [boat, setBoat] = React.useState()
+    const path = usePathname();
     const id = usePathname()
     const {boats} = useStateContext()
+
     React.useEffect(() => {
+        const id = path.split('/').pop()
         const filteredBoat = boats.find(boat => boat._id == id)
-        setBoat(filteredBoat)
+        if(filteredBoat){
+            if(path){
+                const query = `*[_type == "featuredBoats" && _id == $id] {
+                    _id,
+                    title,
+                    stars,
+                    type,
+                    tag,
+                    desc,
+                    architecture,
+                    engex,
+                    dimensions,
+                    img,
+                    imgs,
+                    bookings,
+                    'imgUrl': img.asset->url
+                }`
+                client
+                    .fetch(query, { id })
+                    .then(data => {
+                        setBoat(data)
+                        console.log(data)
+                    })
+                    .catch(error => console.error(error))
+            }
+        }else{
+            setBoat(filteredBoat)
+        }
     }, [])
+    const starArray = boat && new Array(boat.stars).fill(null) || null;
   return (
     <div className='md:px-16 px-5 md:py-20 py-16'>
         {boat && <div className="bg-white w-[100%] rounded-[15px] py-5 md:px-10 px-5">
@@ -43,8 +78,8 @@ const BoatDetails = () => {
                     <div className="my-7">
                         <h3 className="poppins font-semibold text-lg">Dimensions:</h3>
                         <ul>
-                            <li>Length: {boat.dimensions.length} meters</li>
-                            <li>Width: {boat.dimensions.width} meters</li>
+                            <li>Length: {boat.dimensions?.length} meters</li>
+                            <li>Width: {boat.dimensions?.width} meters</li>
                         </ul>
                     </div>
                     <div className="my-7">
@@ -60,7 +95,7 @@ const BoatDetails = () => {
                         <p className="poppins mt-5">{boat.engex}</p>
                     </div>
                     <div className="my-10 grid grid-cols-4 gap-5">
-                        {boat.imgs.map(img => <Image src={img} className="mb-3 w-[100%] h-[200px]" width="100%" height="150" />)}
+                        {boat.imgs?.map(img => <Image src={img} className="mb-3 w-[100%] h-[200px]" width="100%" height="150" />)}
                     </div>
                 </div>
             </div>
