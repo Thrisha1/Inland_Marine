8bafa47ea06719dc1fe3e48776d8286ed85cb21c
Thrisha <thrishakannan25@gmail.com>
2023-09-18 15:37:29 +0530

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,10 @@ export const Navbar = () => {
   const [navbar, setNavbar] = React.useState(false);
   const path = usePathname();
 
+  const navReducer = () => {
+    setNavbar(!navbar);
+  };
+
   const Navs = [
     {
       name: "Home",
@@ -34,65 +38,69 @@ export const Navbar = () => {
     },
   ];
 
-    return (
-        <nav className="w-full bg-[#F9F9F9] shadow">
-            <div className="justify-between px-4 md:items-center md:flex md:pl-8">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                      <div className='flex justify-center items-center'>
-                        <Image src={Logo} width="150" height="150" />
-                        <a href="javascript:void(0)">
-                            <h2 className="text-2xl font-bold text-[#1E3888] poppins">INLAND <br /> MARINE</h2>
-                        </a>
-                      </div>
-                        <div className="md:hidden">
-                            <button
-                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    className={`flex-1 justify-self-center md:block pb-3 mt-8 md:pb-0 md:mt-0 ${
-                        navbar ? "block" : "hidden"
-                    }`}
-                >
-                    <ul className="items-center justify-evenly space-y-8 md:flex md:space-x-6 md:space-y-0">
-                        {Navs.map(nav => <NavBtn setNavbar={setNavbar} name={nav.name} to={nav.to} path={path} />)}
-                    </ul>
-                </div>
+  return (
+    <nav className="w-full bg-[#F9F9F9] shadow">
+      <div className="justify-between px-4 md:items-center md:flex md:pl-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex justify-center items-center">
+              <Image src={Logo} width="150" height="150" />
+              <a href="javascript:void(0)">
+                <h2 className="text-2xl font-bold text-[#1E3888] poppins">
+                  INLAND <br /> MARINE
+                </h2>
+              </a>
             </div>
-        </nav>
-    );
-}
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`flex-1 justify-self-center md:block pb-3 mt-8 md:pb-0 md:mt-0 ${
+            navbar ? "block" : "hidden"
+          }`}
+        >
+          <ul className="flex md:flex-row md:self-end flex-col items-center justify-end space-y-8 md:space-x-6 md:space-y-0">
+            {Navs.map((nav) => (
+              <NavBtn setNavbar={setNavbar} name={nav.name} to={nav.to} path={path} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
